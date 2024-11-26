66abde55ca35c5dc78a603242df22f9640b29e61
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2024-11-26 10:25:39 +0100

    ✨Added rating stars

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 //Shopping product
 const products = [
-    { name: "Christmas Tree 1", category: "Christmas Tree", price: 500, rating: 4.8, 
+    { name: "Christmas Tree 1", category: "Christmas Tree", price: 500, rating: 5, 
       image: { url: "assets/images/Christmas_tree1.avif.avif", width: 300, height: 300, alt: "Picture of a Christmas tree with snow over it in porcelain. In the background are more similar Christmas trees." } },
-    { name: "Christmas Tree 2", category: "Christmas Tree", price: 100, rating: 4.5, 
+    { name: "Christmas Tree 2", category: "Christmas Tree", price: 100, rating: 4, 
       image: { url: "assets/images/Christmas_tree2.avif.avif", width: 300, height: 300, alt: "Picture of a Christmas tree with snow on it and a present under the tree, made in porcelain." } },
-    { name: "Christmas Tree 3", category: "Christmas Tree", price: 200, rating: 4.7, 
+    { name: "Christmas Tree 3", category: "Christmas Tree", price: 200, rating: 4, 
       image: { url: "assets/images/Christmas_tree3.avif.avif", width: 300, height: 300, alt: "Picture of two Christmas trees with snow on them and are decorated in porcelain standing next to each other and under them is a glass box." } },
-    { name: "Gingerbread 1", category: "Gingerbread", price: 300, rating: 4.3, 
+    { name: "Gingerbread 1", category: "Gingerbread", price: 300, rating: 4, 
       image: { url: "assets/images/Gingerbread_1.avif.avif", width: 300, height: 300, alt: "Decorated gingerbread house with a snowman standing outside wearing a red hat and scarf. Made in porcelain." } },
-    { name: "Gingerbread 2", category: "Gingerbread", price: 150, rating: 4.9, 
+    { name: "Gingerbread 2", category: "Gingerbread", price: 150, rating: 5, 
       image: { url: "assets/images/Gingerbread_2-avif.avif", width: 300, height: 300, alt: "Decorated gingerbread house with a gingerbread standing outside the house. Made in porcelain." } },
-    { name: "Gingerbread 3", category: "Gingerbread", price: 250, rating: 4.6, 
+    { name: "Gingerbread 3", category: "Gingerbread", price: 250, rating: 3, 
       image: { url: "assets/images/Gingerbread_3.avif.avif", width: 300, height: 300, alt: "Decorated gingerbread house with Christmas trees on either side of the door outside the house. Made in porcelain." } },
-    { name: "Santa Plate 1", category: "Plate", price: 120, rating: 4.8, 
+    { name: "Santa Plate 1", category: "Plate", price: 120, rating: 2, 
       image: { url: "assets/images/Santa_plate1.avif.avif", width: 300, height: 300, alt: "A Christmas plate with a red border all around and a pattern of a standing happy Santa Claus and around the Santa Claus there is a deer, polka dot pole and Christmas decorations." } },
-    { name: "Santa Plate 2", category: "Plate", price: 200, rating: 4.2, 
+    { name: "Santa Plate 2", category: "Plate", price: 200, rating: 3, 
       image: { url: "assets/images/Santa_plate2.avif.avif", width: 300, height: 300, alt: "Christmas plate with a blue border all around with a Santa's face in the middle and Christmas decoration in the form of deer and polka." } },
-    { name: "Santa Plate 3", category: "Plate", price: 80, rating: 4.7, 
+    { name: "Santa Plate 3", category: "Plate", price: 80, rating: 4, 
       image: { url: "assets/images/Santa_plate3.avif.avif", width: 300, height: 300, alt: "Christmas plate with a red white border all around with Santa in the center of the plate. Around Santa there are Christmas decorations in the form of stars and deer." } },
-    { name: "Gold Christmas Bauble", category: "Bauble", price: 50, rating: 4.4, 
+    { name: "Gold Christmas Bauble", category: "Bauble", price: 50, rating: 2, 
       image: { url: "assets/images/Gold_Christmas_Bauble.avif.avif", width: 300, height: 300, alt: "Close-up of a golden Christmas bauble hanging from a Christmas tree." } },
-    { name: "Red Christmas Bauble", category: "Bauble", price: 300, rating: 4.9, 
+    { name: "Red Christmas Bauble", category: "Bauble", price: 300, rating: 4, 
       image: { url: "assets/images/Red_Christmas_Bauble.avif.avif", width: 300, height: 300, alt: "Close-up of a red and white Christmas bauble hanging from a Christmas tree." } },
-    { name: "Snowflake Christmas Bauble", category: "Bauble", price: 400, rating: 4.5, 
+    { name: "Snowflake Christmas Bauble", category: "Bauble", price: 400, rating: 1, 
       image: { url: "assets/images/Snowflake_Christmas_Bauble.avif.avif", width: 300, height: 300, alt: "Close-up of a snowflake Christmas bauble hanging from a Christmas tree." } },
   ];
 
@@ -158,6 +158,19 @@ function setupAddToCartButtons() {
     });
 }
 
+// Function for generate stars based on rating
+function generateStar(rating) {
+    let stars = '';
+    for (let i = 0; i < 5; i++) {
+        if (i < rating) {
+            stars += '<span class="material-symbols-outlined rating-star">star</span>'; // Filled star
+        } else {
+            stars += '<span class="material-symbols-outlined rating-star-empty">star</span>'; // Empty star
+        }
+    }
+    return stars;
+}
+
 
 // Display all products
 function displayProducts(productsToDisplay) {
@@ -172,7 +185,7 @@ function displayProducts(productsToDisplay) {
             <img src="${product.image.url}" alt="${product.image.alt}" loading="lazy">
             <p>${product.name}</p>
             <p>${product.price} SEK</p>
-            <p>Rating: ${product.rating}</p>
+            <div class="rating">${generateStar(product.rating)}</div>
             <p>${product.category}</p>
             <div class="quantity-controls">
                 <button class="decrease" data-price="${product.price}" data-name="${product.name}">-</button>
